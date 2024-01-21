be3158ceb0da078a2f182b5d37ced51ad152b772
biffislegit <155698316+biffislegit@users.noreply.github.com>
2024-01-21 14:34:10 -0600

    comments

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
 def main()-> None:
+    """create report about specified book"""
     file_path = "./books/frankenstein.txt"
     file_contents = read_file(file_path)
     words_count = count_words(file_contents)
     letter_count = count_letters(file_contents)
-    final_report(file_path, words_count, letter_count)
+    summary = final_report(file_path, words_count, letter_count)
+    for line in summary:
+        print(line)
 
 
-def final_report(book, words, letters):
+def final_report(book: str, words: int, letters: dict) -> list[str]:
+    """generate report summary"""
     title = f"Begin report of {book}"
-    print(f"\n*** {title} ***")
-    print(f"{words} found in the document\n")
+    report = [f"\n*** {title} ***", f"{words} found in the document\n"]
     for letter, counts in letters:
-        print(f"The '{letter}' character was found {counts} times")
-    print("--- End Report ---")
+        report.append(f"The '{letter}' character was found {counts} times")
+    report.append("--- End Report ---")
+    return report
 
 
-def read_file(file_path: str):
+def read_file(file_path: str) -> str:
+    """read and return contents of file"""
     with open(file_path) as f:
         file_contents = f.read()
     return file_contents
 
 
-def count_words(file):
+def count_words(file: str) -> int:
+    """return total word count of file"""
     words = file.split()
     return len(words)
 
 
-def count_letters(file_contents):
+def count_letters(file_contents: str) -> dict:
+    """generate dict of letter with corresponding count"""
     lower_letters = file_contents.lower()
     letters = []
     for letter in lower_letters:
